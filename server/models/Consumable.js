6c01838917b7faf7141dabf5c08a2a917f28fca1
hiteshmaurya56 <118480682+hiteshmaurya56@users.noreply.github.com>
2024-03-12 17:57:23 +0530

    Made Consumable Schema

---
+++ server/models/Consumable.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const ConsumableSchema = new Schema({
+  department: String,
+  budget: Number,
+  expenditure: { type: Number, default: 0 },
+  year: { type: Date, default: () => new Date().getFullYear() },
+  indents_process: [
+    {
+      entry_date: { type: Date, default: Date.now },
+      particulars: String,
+      indenter: String,
+      indent_no: { type: Number, required: true },
+      po_no: Number,
+      indent_amount: Number,
+      amount: Number,
+      account_head: Number,
+    },
+  ],
+  direct_purchase: [
+    {
+      entry_date: { type: Date, default: Date.now },
+      particulars: String,
+      indenter: String,
+      indent_no: { type: Number, required: true },
+      po_no: Number,
+      indent_amount: Number,
+      amount: Number,
+      account_head: Number,
+    },
+  ],
+  indent_pay_done: [
+    {
+      entry_date: { type: Date, default: Date.now },
+      particulars: String,
+      indenter: String,
+      indent_no: { type: Number, required: true },
+      po_no: Number,
+      indent_amount: Number,
+      amount: Number,
+      account_head: Number,
+    },
+  ],
+});
+
+const Consumable = mongoose.model("consumable", ConsumableSchema);
+
+export default Consumable;
