a726ad43553cd0067b3805bd09043ac64a49abd8
hiteshmaurya56 <118480682+hiteshmaurya56@users.noreply.github.com>
2024-03-30 00:18:41 +0530

    Merged

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { body } from "express-validator";
-import { createUser, addDept, increase_budget,newyear, removeUser, updateUser } from "../controllers/adminController.js";
+import { createUser,  increase_budget,newyear, removeUser, updateUser } from "../controllers/adminController.js";
 const router = express.Router();
 
 router.post(
@@ -19,21 +19,21 @@ router.post(
   createUser
 );
 
-router.post(
-  "/adddept",
-  [
-    body("username", "Username should be atleast 2 characters long.").isLength({
-      min: 2,
-    }),
-    body("name", "Name should be atleast 3 characters long. ").isLength({
-      min: 5,
-    }),
-    body("password", "Password should be atleast 6 characters long.").isLength({
-      min: 6,
-    }),
-  ],
-  addDept
-);
+// router.post(
+//   "/adddept",
+//   [
+//     body("username", "Username should be atleast 2 characters long.").isLength({
+//       min: 2,
+//     }),
+//     body("name", "Name should be atleast 3 characters long. ").isLength({
+//       min: 5,
+//     }),
+//     body("password", "Password should be atleast 6 characters long.").isLength({
+//       min: 6,
+//     }),
+//   ],
+//   addDept
+// );
 
 router.post(
   "/increaseBudget",
