c07a76cbd1499a190ce02b773f4f215ad53eb069
hiteshmaurya56 <118480682+hiteshmaurya56@users.noreply.github.com>
2024-05-06 11:05:05 +0530

    Minor Changes

--- client/src/department/Navbar/Navbar.js
+++ client/src/department/Navbar/Navbar.js
@@ -54,7 +54,7 @@ const Navbar = () => {
                     <Link
                       className="nav-link active"
                       aria-current="page"
-                      to="/"
+                      to="/dept"
                     >
                       Home |
                     </Link>
