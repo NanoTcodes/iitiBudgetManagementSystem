4f83db13c0248b77a0e93dd29ccd9b40de71fa87
NanoTcodes <NanoTcodes@gmail.com>
2024-05-06 11:03:38 +0530

    check

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
 console.log(`${process.env.REACT_APP_API_HOST}/api/user/login`)
   const login = async (token) => {
     const response = await fetch(
-      `https://${process.env.REACT_APP_API_HOST}/api/user/login`,
+      `${process.env.REACT_APP_API_HOST}/api/user/login`,
       {
         method: "POST",
         headers: {
